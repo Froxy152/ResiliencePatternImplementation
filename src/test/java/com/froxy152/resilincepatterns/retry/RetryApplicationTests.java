b9bfaffa1a69fcfff61178fc70c092cfad32dcca
Froxy152 <rukyss.games.91@mail.ru>
2026-02-18 12:08:40 +0100

    init commit

---
+++ src/test/java/com/froxy152/resilincepatterns/retry/RetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.froxy152.resilincepatterns.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
